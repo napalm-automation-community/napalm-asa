5af99d927ca6a410aff3ab237a413dafdf36f4cc
Diogo Assumpcao <diogo@regattapix.com>
2018-06-08 10:26:03 -0300

    Add support to additional interface types

--- napalm_asa/_SUPPORTED_INTERFACES_ENDPOINTS.py
+++ napalm_asa/_SUPPORTED_INTERFACES_ENDPOINTS.py
@@ -1,8 +1,6 @@
 SUPPORTED_INTERFACES_ENDPOINTS = (
     '/interfaces/physical',
-    '/interfaces/vlan'
+    '/interfaces/vlan',
+    '/interfaces/redundant',
+    '/interfaces/portchannel'
 )
-
-# , '/interfaces/portchannel',
-#  '/interfaces/redundant', '/interfaces/bvi', '/api/interfaces/setup',
-#  '/interfaces/firepower/93xx/portchannel')
