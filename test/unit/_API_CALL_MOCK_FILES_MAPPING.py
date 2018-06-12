08eeb960f498465c306b7f331ec92ae1b02671e4
Diogo Assumpcao <diogo@regattapix.com>
2018-06-12 10:26:44 -0300

    Fix linting issues

--- test/unit/_API_CALL_MOCK_FILES_MAPPING.py
+++ test/unit/_API_CALL_MOCK_FILES_MAPPING.py
@@ -2,7 +2,8 @@ API_CALL_MOCK_FILES_MAPPING = {
     '_cli_show_hostname_show_hostname_fqdn': '_cli_show_hostname_show_hostname_fqdn.json',
     '_cli_show_running-config': '_cli_show_running-config.json',
     '_cli_show_startup-config': '_cli_show_startup-config.json',
-    '_cli_show_startup-config_show_running-config': '_cli_show_startup-config_show_running-config.json',
+    '_cli_show_startup-config_show_running-config':
+        '_cli_show_startup-config_show_running-config.json',
     '_interfaces_physical': '_interfaces_physical.json',
     '_interfaces_portchannel': '_interfaces_portchannel.json',
     '_interfaces_redundant': '_interfaces_redundant.json',
@@ -10,5 +11,7 @@ API_CALL_MOCK_FILES_MAPPING = {
     '_monitoring_arp': '_monitoring_arp.json',
     '_monitoring_device_components_version': '_monitoring_device_components_version.json',
     '_monitoring_serialnumber': '_monitoring_serialnumber.json',
-    '_cli_show_interface_GigabitEthernet0_0_show_interface_Management0_0_show_interface_GigabitEthernet0_1_show_interface_Management0_0.2_show_interface_Redundant1_show_interface_Port-channel1_show_interface_Port-channel2' : '_cli_get_interfaces_multiple.json'
+    "_cli_show_interface_GigabitEthernet0_0_show_interface_Management0_0_show_interface_\
+GigabitEthernet0_1_show_interface_Management0_0.2_show_interface_Redundant1_show_\
+interface_Port-channel1_show_interface_Port-channel2": '_cli_get_interfaces_multiple.json'
 }
