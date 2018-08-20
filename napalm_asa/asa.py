96d6431dbabd13ddc13a173cf91f3139af5f303a
Diogo Assumpcao <diogo@regattapix.com>
2018-08-20 04:09:53 -0300

    Initial work to use ASDM interface instead of REST API

--- napalm_asa/asa.py
+++ napalm_asa/asa.py
@@ -23,8 +23,11 @@ from __future__ import unicode_literals
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
+import base64
 import re
+from string import whitespace
 from collections import OrderedDict
+from netaddr import IPNetwork
 
 # import third party lib
 from netaddr import IPNetwork
@@ -37,15 +40,16 @@ from napalm.base.exceptions import (
 )
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
+IPV4_ADDR_REGEX = r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
 class RespFetcherHttps:
     """Response fetcher."""
 
     def __init__(
         self,
-        username='admin',
-        password='insieme',
-        base_url='https://172.21.128.227/api',
+        username='cisco',
+        password='cisco',
+        base_url='https://172.21.128.227/admin/config',
         timeout=30
     ):
         """Class init."""
@@ -55,49 +59,30 @@ class RespFetcherHttps:
         self.timeout = timeout
         self.token = ""
         self.session = requests.Session()
-        self.headers = {'Content-Type': 'application/json'}
+        self.headers = {'Content-Type': 'text/xml'}
 
-    def get_auth_token(self):
-        """ Authenticate with user and password to get an auth token."""
-        full_url = self.base_url + "/tokenservices"
-        try:
-            token_request = self.session.post(full_url, auth=(self.username, self.password),
-                                              data="", timeout=self.timeout, verify=False)
-            if token_request.status_code is 204 and 'X-Auth-Token' in token_request.headers.keys():
-                self.token = token_request.headers['X-Auth-Token']
-                self.session.headers.update({'X-Auth-Token': token_request.headers['X-Auth-Token']})
-                return (True, None)
-            else:
-                return (False, token_request.status_code)
-        except requests.exceptions.RequestException as e:
-            raise ConnectionException(py23_compat.text_type(e))
+    def test_connection(self):
+        """ Test connection to ASA via the ASDM Interface"""
+        response = self.get_resp(endpoint="/show+version", returnObject=True)
 
-    def delete_token(self):
-        """Delete auth token."""
-        full_url = self.base_url + "/tokenservices/{}".format(self.token)
-        try:
-            token_delete_request = self.session.delete(full_url,
-                                                       auth=(self.username, self.password),
-                                                       timeout=self.timeout, verify=False)
-            if token_delete_request.status_code is 204:
-                self.session.headers.pop('X-Auth-Token', None)
-                return (True, None)
-            else:
-                return (False, token_delete_request.status_code)
-        except requests.exceptions.RequestException as e:
-            raise ConnectionException(py23_compat.text_type(e))
+        if response.status_code is 200:
+            return (True, 200)
+        else:
+            return (False, response.status_code)
 
-    def get_resp(self, endpoint="", data=None, params={}, throw=True):
+        return response 
+        
+    def get_resp(self, endpoint="", data=None, params={}, throw=True, returnObject=False):
         """Get response from device and returne parsed json."""
         full_url = self.base_url + endpoint
         f = None
         try:
             if data is not None:
-                f = self.session.post(full_url, data=data,
+                f = self.session.post(full_url, data=data, auth=(self.username, self.password),
                                       headers=self.headers, timeout=self.timeout,
                                       params=params, verify=False)
             else:
-                f = self.session.get(full_url,
+                f = self.session.get(full_url, auth=(self.username, self.password),
                                      headers=self.headers, timeout=self.timeout,
                                      params=params, verify=False)
             if (f.status_code != 200):
@@ -107,22 +92,15 @@ class RespFetcherHttps:
                 else:
                     return False
 
-            return f.json()
+            if returnObject:
+                return f
+            else:
+                return f.text
         except requests.exceptions.RequestException as e:
             if throw:
                 raise ConnectionException(py23_compat.text_type(e))
             else:
                 return False
-
-    def has_active_token(self):
-        status = False
-        if "X-Auth-Token" in self.session.headers:
-                response = self.get_resp('/monitoring/serialnumber',  throw=False)
-                if 'kind' in response and response['kind'] == "object#QuerySerialNumber":
-                    status = True
-
-        return status
-
 
 class ASADriver(NetworkDriver):
     """Napalm driver for Cisco ASA."""
@@ -141,21 +119,14 @@ class ASADriver(NetworkDriver):
         self.port = optional_args.get('port', 443)
         self.timeout = timeout
         self.up = False
-        self.base_url = "https://{}:{}/api".format(self.hostname, self.port)
+        self.base_url = "https://{}:{}/admin/exec".format(self.hostname, self.port)
         self.device = RespFetcherHttps(self.username, self.password, self.base_url, self.timeout)
 
     def _authenticate(self):
         """Authenticate with device."""
-        auth_result = self.device.get_auth_token()
+        auth_result = self.device.test_connection()
 
         return auth_result
-
-    def _delete_token(self):
-        """Delete auth token."""
-
-        delete_result = self.device.delete_token()
-
-        return delete_result
 
     def _send_request(self, endpoint, data=None):
         """Send request method."""
@@ -164,47 +135,7 @@ class ASADriver(NetworkDriver):
         else:
             response = self.device.get_resp(endpoint, json.dumps(data))
 
-        if 'rangeInfo' in response:
-            if response['rangeInfo']['limit'] < response['rangeInfo']['total']:
-                fetched_items = len(response['items'])
-                while fetched_items < response['rangeInfo']['total']:
-                    offset = fetched_items
-                    params = {'offset': offset}
-                    if data is None:
-                        r = self.device.get_resp(endpoint=endpoint, params=params)
-                    else:
-                        r = self.device.get_resp(endpoint=endpoint, data=json.dumps(data),
-                                                 params=params)
-
-                    fetched_items = fetched_items + len(r['items'])
-                    response['items'] = response['items'] + r['items']
-
         return response
-
-    def _get_interfaces_details(self, interfaces):
-        commands = []
-        for interface in interfaces:
-            commands.append("show interface " + interface)
-
-        results = self.cli(commands)
-
-        ifs_details = {}
-        for command, details in results.items():
-            if_name = re.search(r"show interface (.*)", command).group(1)
-            mac = re.search(r"MAC address (.{14}),", details).group(1)
-
-            match_if_status = re.search(r"line protocol is (.{2,4})\n", results[command])
-            if match_if_status.group(1) == 'up':
-                if_up = True
-            else:
-                if_up = False
-
-            ifs_details[if_name] = {
-                                'mac_address': mac,
-                                'is_up': if_up
-                                }
-
-        return ifs_details
 
     def open(self):
         """
@@ -224,13 +155,9 @@ class ASADriver(NetworkDriver):
 
     def close(self):
         """Mark the connection to the device as closed."""
-        delete_result, code = self._delete_token()
 
-        if delete_result:
-            self.up = False
-            return True
-        else:
-            raise ConnectionException('Cannot connect to {}. Error {}'.format(self.hostname, code))
+        self.up = False
+        return True
 
     def cli(self, commands):
         """Run CLI commands via the API."""
@@ -247,149 +174,34 @@ class ASADriver(NetworkDriver):
 
         return result_dict
 
-    def get_facts(self):
-        """Get Facts."""
-        facts = {
-                'uptime': 0.0,
-                'vendor': u'Cisco Systems',
-                'os_version': u'',
-                'serial_number': u'',
-                'model': u'',
-                'hostname': u'',
-                'fqdn': u'',
-                'interface_list': []
-                }
-
-        serialNumber = self._send_request('/monitoring/serialnumber')
-        facts['serial_number'] = serialNumber['serialNumber']
-
-        deviceDetails = self._send_request('/monitoring/device/components/version')
-        facts['os_version'] = deviceDetails['asaVersion']
-        facts['uptime'] = deviceDetails['upTimeinSeconds']
-        facts['model'] = deviceDetails['deviceType']
-
-        results_from_cli = self.cli(['show hostname', 'show hostname fqdn'])
-        facts['hostname'] = results_from_cli['show hostname'].replace('\n', '')
-        facts['fqdn'] = results_from_cli['show hostname fqdn'].replace('\n', '')
-
-        interfaces = self.get_interfaces()
-
-        for if_name in interfaces:
-            facts['interface_list'].append(if_name)
-
-        return facts
-
-    def get_interfaces(self):
-        """Get Interfaces."""
-        interfaces = OrderedDict()
-        response = self._send_request('/interfaces/physical')
-
-        if response['rangeInfo']['total'] > 0:
-
-            for int_info in response['items']:
-                interfaces[int_info['hardwareID']] = {
-                    'is_up': False,
-                    'is_enabled': not int_info['shutdown'],
-                    'description': int_info['interfaceDesc'],
-                    'last_flapped': -1.0,
-                    'speed': 0,
-                    'mac_address': u'',
-                }
-
-            ifs = []
-            for if_name in interfaces:
-                ifs.append(if_name)
-
-            ifs_details = self._get_interfaces_details(ifs)
-
-            for if_name, details in ifs_details.items():
-                interfaces[if_name]['mac_address'] = details['mac_address']
-                interfaces[if_name]['is_up'] = details['is_up']
-
-        return interfaces
-
-    def get_config(self, retrieve='all'):
-        """Get config."""
-        config = {
-            'startup': '',
-            'running': '',
-            'candidate': ''
-        }
-
-        commands = []
-        startup_cmd = "show startup-config"
-        running_cmd = "show running-config"
-
-        if retrieve.lower() in ['startup', 'all']:
-            commands.append(startup_cmd)
-        if retrieve.lower() in ['running', 'all']:
-            commands.append(running_cmd)
-
-        if retrieve.lower() in ['running', 'startup', 'all']:
-            results = self.cli(commands)
-
-        if retrieve.lower() in ['startup', 'all']:
-            config['startup'] = results[startup_cmd]
-        if retrieve.lower() in ['running', 'all']:
-            config['running'] = results[running_cmd]
-
-        return config
-
     def get_interfaces_ip(self):
         """Get interfaces ip."""
         interfaces = {}
-        response = self._send_request('/interfaces/physical')
+        show_interface = self._send_request('/show+interface')
 
-        if response['rangeInfo']['total'] > 0:
+        INTERNET_ADDRESS = r'\s+(?:IP address|Secondary address)'
+        INTERNET_ADDRESS += r' (?P<ip>{}), subnet mask (?P<mask>{})'.format(IPV4_ADDR_REGEX, IPV4_ADDR_REGEX)
+        # LINK_LOCAL_ADDRESS = r'\s+IPv6 is enabled, link-local address is (?P<ip>[a-fA-F0-9:]+)'
+        # GLOBAL_ADDRESS = r'\s+(?P<ip>[a-fA-F0-9:]+), subnet is (?:[a-fA-F0-9:]+)/(?P<prefix>\d+)'
 
-            for int_info in response['items']:
-
-                if int_info['ipAddress'] != "NoneSelected":
-                    interfaces[int_info['hardwareID']] = {}
-                    ipv4 = int_info['ipAddress']
-                    ip = ipv4['ip']['value']
-                    mask = ipv4['netMask']['value']
-                    network = ip + '/' + mask
-                    prefix_length = IPNetwork(network).prefixlen
-                    interfaces[int_info['hardwareID']]['ipv4'] = \
-                        {ip: {'prefix_length': prefix_length}}
-
-                if len(int_info['ipv6Info']['ipv6Addresses']) > 0:
-                    if int_info['hardwareID'] not in interfaces:
-                        interfaces[int_info['hardwareID']] = {}
-
-                    interfaces[int_info['hardwareID']]['ipv6'] = {}
-                    for ipv6 in int_info['ipv6Info']['ipv6Addresses']:
-                        ip = ipv6['address']['value']
-                        prefix_length = ipv6['prefixLength']
-                        interfaces[int_info['hardwareID']]['ipv6'][ip] = \
-                            {'prefix_length': prefix_length}
+        for line in show_interface.splitlines():
+            if(len(line.strip()) == 0):
+                continue
+            if(line[0] not in whitespace):
+                ipv4 = {}
+                interface_name = line.split()[1]
+            m = re.match(INTERNET_ADDRESS, line)
+            if m:
+                ip, prefix = m.groups()
+                ip_network = IPNetwork("{}/{}".format(ip,prefix))
+                ipv4.update({ip: {"prefix_length": ip_network.prefixlen}})
+                interfaces[interface_name] = {'ipv4': ipv4}
 
         return interfaces
 
-    def get_arp_table(self):
-        """Get ARP Table."""
-        arp_table = []
-        response = self._send_request('/monitoring/arp')
-
-        if response['rangeInfo']['total'] > 0:
-            for item in response['items']:
-                mac = item['macAddress'].replace('.', '')
-                regex = re.compile(r'.{2}')
-                mac = ":".join(re.findall(regex, mac))
-                arp_table.append(
-                    {
-                        'interface': item['interface'],
-                        'ip': item['ipAddress'],
-                        'mac': mac,
-                        'age': 0.0
-                    }
-                )
-
-        return arp_table
-
     def is_alive(self):
         """Check if connection is still valid."""
-        status = {"is_alive": self.device.has_active_token()}
+        alive, code = self.device.test_connection()
+        status = {"is_alive": alive }
 
         return status
